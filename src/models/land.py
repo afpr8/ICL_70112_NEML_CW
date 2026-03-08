[fix] Fixed the functionality of the learning rate scaling to not take an update that worsens the loss. Introduced a patience argument to the class for the fit method. Batched the initial computation of the log_maps paths. Removed redundant log maps calculation

src/models/land.py
@@ -3,7 +3,10 @@ import jax.numpy as jnp
 from tqdm import tqdm
 import numpy as np
 
-from src.utils.land_utils import RiemannianManifold, compute_knn_initial_path
+from src.utils.land_utils import (
+    RiemannianManifold,
+    compute_knn_initial_paths,
+)
 
 
 class LANDMLE:
@@ -23,9 +26,11 @@ class LANDMLE:
         lr_scale_down: float = 0.75,  # 0.75 as in the original LAND paper
         lr_scale_up: float = 1.1,  # 1.1 as in the original LAND paper
         epsilon: float = 1e-3,
+        patience: int = 10,
         sigma: float = 1.0,  # Set to 1.0 in the original paper and tested from 0.5 to 1.5
         rho: float = 1e-3,
         K_segments: int = 5,
+        n_neighbors: int = 5,
         init_method: str = "mean",
         seed: int = 42,
     ):
@@ -53,9 +58,11 @@ class LANDMLE:
         self.lr_scale_up = lr_scale_up
         self.S = S
         self.epsilon = epsilon
+        self.patience = patience
         self.sigma = sigma
         self.rho = rho
         self.K_segments = K_segments
+        self.n_neighbors = n_neighbors
         self.init_method = init_method
         self.key = jax.random.key(seed)
 
@@ -69,91 +76,127 @@ class LANDMLE:
             sigma (jnp.ndarray): The covariance of the distribution
             normalization_constant (jnp.ndarray): The normalization constant of the distribution
         """
-        manifold = RiemannianManifold(X, self.sigma, self.rho, self.K_segments)
-
-        self.key, subkey = jax.random.split(self.key)
-        mu, A, sigma = self._init_params(X, subkey, self.init_method, manifold)
-
-        self.key, subkey = jax.random.split(self.key)
-        norm_const, v_samples = manifold.compute_normalization_constant(
-            mu, sigma, subkey, self.S
+        manifold = RiemannianManifold(
+            X, self.sigma, self.rho, self.K_segments, self.n_neighbors
         )
-        loss_diff = float("inf")
 
+        self.key, subkey = jax.random.split(self.key)
+        mu_t1, A_t2, sigma_t2 = self._init_params(X, subkey, self.init_method, manifold)
+
+        self.key, subkey = jax.random.split(self.key)
+        norm_const_t2, v_samples_t2 = manifold.compute_normalization_constant(
+            mu_t1, sigma_t2, subkey, self.S
+        )
+        log_maps_t1 = self._compute_log_maps(mu_t1, X, manifold)
+
+        loss_t2 = self._loss(sigma_t2, log_maps_t1, norm_const_t2)
+        loss_diff = loss_t2
+
+        n_wo_improvement = 0
         with tqdm(desc="LAND MLE Fit", unit="epoch") as pbar:
-            while loss_diff**2 > self.epsilon:
+            while loss_diff > self.epsilon and n_wo_improvement < self.patience:
                 # Store previous values
-                prev_mu = mu
-                prev_A = A
-                prev_sigma = sigma
-                prev_norm_const = norm_const
-
-                log_maps = self._compute_log_maps(mu, X, manifold)
-                prev_loss = self._loss(sigma, log_maps, norm_const)
+                mu_t = mu_t1
+                A_t = A_t2
+                sigma_t = sigma_t2
+                norm_const_t = norm_const_t2
+                v_samples_t = v_samples_t2
+                loss_t = loss_t2
+                log_maps_t = log_maps_t1  # log maps only depend on mu so only 1 update
 
                 # Update mu
                 self.key, subkey = jax.random.split(self.key)
                 grad_mu = self._compute_grad_mu(
-                    mu, sigma, norm_const, v_samples, subkey, log_maps, manifold
+                    mu_t,
+                    sigma_t,
+                    norm_const_t,
+                    v_samples_t,
+                    subkey,
+                    log_maps_t,
+                    manifold,
                 )
-                mu = manifold.exp_map(mu, self.lr_mu * grad_mu)
+                mu_t1 = manifold.exp_map(mu_t, self.lr_mu * grad_mu)
 
                 self.key, subkey = jax.random.split(self.key)
-                norm_const, v_samples = manifold.compute_normalization_constant(
-                    mu, sigma, subkey, self.S
+                norm_const_t1, v_samples_t1 = manifold.compute_normalization_constant(
+                    mu_t1, sigma_t, subkey, self.S
                 )
+                log_maps_t1 = self._compute_log_maps(mu_t1, X, manifold)
+                loss_t1 = self._loss(sigma_t, log_maps_t1, norm_const_t1)
 
                 # Scale lr_mu
-                log_maps = self._compute_log_maps(mu, X, manifold)
-                loss_diff = self._loss(sigma, log_maps, norm_const) - prev_loss
-                if loss_diff > 0:
+                if loss_t1 - loss_t > 0:
                     self.lr_mu *= self.lr_scale_down
-                    mu = prev_mu
+                    # Revert to previous values
+                    mu_t1 = mu_t
+                    norm_const_t1 = norm_const_t
+                    v_samples_t1 = v_samples_t
+                    loss_t1 = loss_t
+                    log_maps_t1 = log_maps_t
+
                 else:
                     self.lr_mu *= self.lr_scale_up
 
                 # Update sigma
                 self.key, subkey = jax.random.split(self.key)
                 grad_sigma = self._compute_grad_sigma(
-                    mu, A, sigma, norm_const, v_samples, subkey, log_maps, manifold
+                    mu_t1,
+                    A_t,
+                    sigma_t,
+                    norm_const_t1,
+                    v_samples_t1,
+                    subkey,
+                    log_maps_t1,
+                    manifold,
                 )
-                A = A - self.lr_A * grad_sigma
-                sigma = jnp.linalg.inv(A.T @ A)
+                A_t2 = A_t - self.lr_A * grad_sigma
+                sigma_t2 = jnp.linalg.inv(A_t2.T @ A_t2)
 
-                prev_norm_const = norm_const
                 self.key, subkey = jax.random.split(self.key)
-                norm_const, v_samples = manifold.compute_normalization_constant(
-                    mu, sigma, subkey, self.S
+                norm_const_t2, v_samples_t2 = manifold.compute_normalization_constant(
+                    mu_t1, sigma_t2, subkey, self.S
                 )
+                loss_t2 = self._loss(sigma_t2, log_maps_t1, norm_const_t2)
+                loss_diff = abs(loss_t2 - loss_t)
 
                 # Scale lr_A
-                new_loss = self._loss(sigma, log_maps, norm_const)
-                loss_diff_A = new_loss - self._loss(
-                    prev_sigma, log_maps, prev_norm_const
-                )
-                if loss_diff_A > 0:
+                if loss_t2 - loss_t1 > 0:
                     self.lr_A *= self.lr_scale_down
-                    A = prev_A
-                    sigma = prev_sigma
+                    # Revert to previous values
+                    A_t2 = A_t
+                    sigma_t2 = sigma_t
+                    norm_const_t2 = norm_const_t1
+                    v_samples_t2 = v_samples_t1
+                    loss_t2 = loss_t1
                 else:
                     self.lr_A *= self.lr_scale_up
 
-                loss_diff = new_loss - prev_loss
+                # Check for no improvement
+                if loss_t2 == loss_t:
+                    n_wo_improvement += 1
+                else:
+                    n_wo_improvement = 0
 
-                pbar.set_postfix(loss_diff=float(loss_diff), loss=float(new_loss))
+                pbar.set_postfix(
+                    loss_diff=float(loss_diff),
+                    lr_mu=float(self.lr_mu),
+                    lr_A=float(self.lr_A),
+                    loss=float(loss_t2),
+                    no_impr=int(n_wo_improvement),
+                )
                 pbar.update(1)
 
-        return mu, sigma, norm_const
+        return mu_t1, sigma_t2, norm_const_t2
 
     def _compute_log_maps(
         self, mu: jnp.ndarray, X: jnp.ndarray, manifold: RiemannianManifold
     ) -> jnp.ndarray:
         m_np = np.array(mu)
         X_np = np.array(X)
-        paths = [
-            compute_knn_initial_path(m_np, X_np[i], X_np, N_points=self.K_segments + 1)
-            for i in range(X_np.shape[0])
-        ]
+        # Batch compute initial paths using Riemannian weights to avoid gaps in topology
+        paths = compute_knn_initial_paths(
+            m_np, X_np, manifold, N_points=self.K_segments + 1
+        )
         return manifold.log_map_batch(mu, X, jnp.array(paths))
 
     def _init_params(