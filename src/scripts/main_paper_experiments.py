462f3cee0e5836e148b1c18d98275b40a449156e
winoo19 <danielsanchezs1903@gmail.com>
2026-03-09 22:49:55 +0000

    [code] full experiments from land paper

---
+++ src/scripts/main_paper_experiments.py
@@ -0,0 +1,5 @@
+from src.experiments.land_paper_full import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/scripts/main_synthetic_experiments.py
@@ -0,0 +1,5 @@
+from src.experiments.synthetic_land_paper import main
+
+
+if __name__ == "__main__":
+    main()
